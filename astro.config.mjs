1dc27e219aade170592001c2b07f05849cbbd06b
Atllas1981 <tu-email@dominio.com>
2025-10-28 08:00:44 -0500

    Configurar despliegue a GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://atllas1981.github.io/deployarCon-Astro/', // URL completa del sitio
+  base: '/deployarCon-Astro/', // nombre exacto del repositorio
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
